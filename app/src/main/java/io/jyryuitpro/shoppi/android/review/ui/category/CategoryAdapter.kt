7ff554a32ee08a75e0fcc1298f031439ca1c389e
jyr <jyr@wavers.co.kr>
2022-09-17 16:58:12 +0900

    복습 및 실습 : RecyclerView - 1. item layout 구현, 실습 : RecyclerView - 2. Adapter 구현 with data binding

---
+++ app/src/main/java/io/jyryuitpro/shoppi/android/review/ui/category/CategoryAdapter.kt
@@ -0,0 +1,40 @@
+package io.jyryuitpro.shoppi.android.review.ui.category
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import io.jyryuitpro.shoppi.android.review.databinding.ItemCategoryBinding
+import io.jyryuitpro.shoppi.android.review.model.Category
+
+class CategoryAdapter : ListAdapter<Category, CategoryAdapter.CategoryViewHolder>(CategoryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        return holder.bind(getItem(position))
+    }
+
+    class CategoryViewHolder(private val binding: ItemCategoryBinding) : ViewHolder(binding.root) {
+
+        fun bind(category: Category) {
+            binding.category = category
+            // 바인딩된 데이터가 바로 뷰에 반영됩니다.
+            binding.executePendingBindings()
+        }
+    }
+}
+
+class CategoryDiffCallback : DiffUtil.ItemCallback<Category>() {
+    override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem.categoryId == newItem.categoryId
+    }
+
+    override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/io/jyryuitpro/shoppi/android/review/ui/category/CategoryFragment.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/android/review/ui/category/CategoryFragment.kt
@@ -8,25 +8,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import io.jyryuitpro.shoppi.android.review.R
+import io.jyryuitpro.shoppi.android.review.databinding.FragmentCategoryBinding
 import io.jyryuitpro.shoppi.android.review.ui.common.ViewModelFactory
 
 class CategoryFragment : Fragment() {
 
     private val viewModel: CategoryViewModel by viewModels { ViewModelFactory(requireContext()) }
+    private lateinit var binding: FragmentCategoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        // return inflater.inflate(R.layout.fragment_category, container, false)
+        binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val categoryAdapter = CategoryAdapter()
+        binding.rvCategoryList.adapter = categoryAdapter
+
         viewModel.items.observe(viewLifecycleOwner) {
-            Log.d("CategoryFragment", "items=$it")
+            categoryAdapter.submitList(it)
         }
     }
 }
